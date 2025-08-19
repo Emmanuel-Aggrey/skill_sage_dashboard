c9347cc6f4c68a231b3ffa323972cde00702a0d5
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2025-08-19 02:26:50 +0000

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
 export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+export const VITE_SERVER_URL = process.env.VITE_SERVER_URL;
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+// const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+// const VITE_SERVER_URL = process.env.VITE_SERVER_URL;
 
+const SERVER_URL = "https://skill-sage-backend-njh6.onrender.com";
 export const client = axios.create({
-  baseURL: REACT_APP_SERVER_URL,
+  baseURL: SERVER_URL,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
