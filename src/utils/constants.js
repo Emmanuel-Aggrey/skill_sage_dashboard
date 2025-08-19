e12122499f05143477aaab872f15b84874da4288
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2025-08-19 02:59:34 +0000

    Updated envs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,11 @@
-const SERVER_URL = "https://skill-sage-backend-njh6.onrender.com";
-
-export const REACT_APP_SERVER_URL = SERVER_URL;
-export const VITE_SERVER_URL = SERVER_URL;
-export const SERVER_BASE_URL = SERVER_URL;
-
-REACT_APP_SERVER_URL = SERVER_URL;
-REACT_APP_LOCAL_BASE_URL = SERVER_URL;
-REACT_APP_SERVER_BASE_URL = SERVER_URL;
+export const REACT_APP_SERVER_URL =
+  process.env.REACT_APP_SERVER_URL ||
+  "https://skill-sage-backend-njh6.onrender.com";
+export const VITE_SERVER_URL =
+  process.env.REACT_APP_SERVER_URL ||
+  "https://skill-sage-backend-njh6.onrender.com";
+export const SERVER_BASE_URL =
+  process.env.REACT_APP_SERVER_URL ||
+  "https://skill-sage-backend-njh6.onrender.com";
+export const REACT_APP_LOCAL_BASE_URL =
+  process.env.REACT_APP_LOCAL_BASE_URL || "http://localhost:5000";
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const SERVER_URL = "https://skill-sage-backend-njh6.onrender.com";
-
-const REACT_APP_SERVER_URL = SERVER_URL;
-const REACT_APP_LOCAL_BASE_URL = SERVER_URL;
-const REACT_APP_SERVER_BASE_URL = SERVER_URL;
+const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL ||
+  "https://skill-sage-backend-njh6.onrender.com";
 
 export const client = axios.create({
   baseURL: SERVER_URL,
